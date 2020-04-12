aaf21b3f394cf3dfefc9b82c94349801b5ce239e
StefanMladenovski97 <63462619+StefanMladenovski97@users.noreply.github.com>
2020-04-12 20:58:56 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -4,9 +4,20 @@ class Student {
 	String lastName;
 
 	//TODO constructor
+	public Student(String i, String fn, String ln){
+		index=i;
+		firstName=fn;
+		lastName=ln;
+	}
+	List<Integer> labPoints = new ArrayList<Integer>();
+	labPoints.add(5);
+	labPoints.add(8);
+	labPoints.add(6);
+	labPoints.add(7);
+	labPoints.add(9);
 
 	//TODO seters & getters
-
+	public double set
 	public double getAverage() {
 		//TODO
 	}
